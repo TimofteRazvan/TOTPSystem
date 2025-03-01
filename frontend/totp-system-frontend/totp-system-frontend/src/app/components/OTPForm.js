f5a85a8fb45a7e5b72919c229efc1d645b297a6a
Timofte Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2025-03-01 17:48:55 +0200

    beautified the page (using the default nextjs page)

--- frontend/totp-system-frontend/totp-system-frontend/src/app/components/OTPForm.js
+++ frontend/totp-system-frontend/totp-system-frontend/src/app/components/OTPForm.js
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import { generateOTP, validateOTP } from "../services/otpService";
 import Swal from "sweetalert2";
+import Image from "next/image";
+import "../styles/globals.css";
 
 export default function OTPForm() {
   const [otp, setOTP] = useState("");
@@ -19,7 +21,7 @@ export default function OTPForm() {
     icon,
     timer,
     timerProgressBar = true,
-    position = "center"
+    position = "top"
   ) {
     Swal.fire({
       toast: true,
@@ -49,50 +51,73 @@ export default function OTPForm() {
         "info",
         expiresIn,
         true,
-        "bottom"
+        "top"
       );
     } catch (err) {
-      notifyToast(`<p>${err.message}</p>`, "error", 3000, true, "center");
+      notifyToast(`<p>${err.message}</p>`, "error", 3000, true, "top");
     }
   };
 
   /**
-   * Handles the OTP validation by calling validateOTP service and displaying the result in a toast notification.
+   * Handles the OTP validation by calling validate OTP service and displaying the result in a toast notification.
    * @returns void
    */
   const handleValidateOTP = async () => {
     if (!otp) {
-      notifyToast(`<p>OTP is required</p>`, "error", 3000, true, "center");
+      notifyToast(`<p>OTP is required</p>`, "error", 3000, true, "top");
       return;
     }
 
     try {
       const response = await validateOTP(otp);
-      notifyToast(
-        `<p>${response.message}</p>`,
-        "success",
-        3000,
-        true,
-        "center"
-      );
+      notifyToast(`<p>${response.message}</p>`, "success", 3000, true, "top");
     } catch (err) {
-      notifyToast(`<p>${err.message}</p>`, "error", 3000, true, "center");
+      notifyToast(`<p>${err.message}</p>`, "error", 3000, true, "top");
     }
   };
 
   return (
     <div>
-      <div>
-        <label>OTP:</label>
-        <input
-          type="text"
-          value={otp}
-          onChange={(e) => setOTP(e.target.value)}
-          placeholder="Enter OTP"
-        />
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1>OTP SYSTEM</h1>
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <div>
+            <label className="input-label">OTP: </label>
+            <input
+              type="text"
+              value={otp}
+              onChange={(e) => setOTP(e.target.value)}
+              placeholder="Enter OTP"
+              className="input-field"
+            />
+          </div>
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={handleGenerateOTP}
+            >
+              <Image
+                className="dark:invert"
+                src="/vercel.svg"
+                alt="Vercel logomark"
+                width={20}
+                height={20}
+              />
+              Generate OTP
+            </a>
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={handleValidateOTP}
+            >
+              Validate OTP
+            </a>
+          </div>
+        </main>
       </div>
-      <button onClick={handleGenerateOTP}>Generate OTP</button>
-      <button onClick={handleValidateOTP}>Validate OTP</button>
     </div>
   );
 }
--- frontend/totp-system-frontend/totp-system-frontend/src/app/page.js
+++ frontend/totp-system-frontend/totp-system-frontend/src/app/page.js
@@ -2,7 +2,6 @@ import OTPForm from "./components/OTPForm";
 export default function Home() {
   return (
     <div className="container">
-      <h1>OTP System</h1>
       <OTPForm />
     </div>
   );
