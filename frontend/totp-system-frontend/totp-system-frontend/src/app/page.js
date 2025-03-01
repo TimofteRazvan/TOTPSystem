f95f1bb6dcf7f6bf05ceecb4260f808f08cadcc8
Timofte Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2025-03-01 16:54:54 +0200

    switched from old deprecated react installation to nextjs framework

---
+++ frontend/totp-system-frontend/totp-system-frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import OTPForm from './components/OTPForm';
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>OTP System</h1>
+      <OTPForm />
+    </div>
+  );
+}
